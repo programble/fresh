090205530bf6da3d38d408574709b5af32c3b731
Curtis McEnroe <programble@gmail.com>
2016-01-17 19:23:30 -0500

    Remove bin

--- src/bin/fresh.rs
+++
@@ -1,9 +0,0 @@
-extern crate fresh;
-
-use fresh::generator::*;
-
-fn main() {
-    println!("{}", Char('a').generate(8));
-    println!("{}", Hex.generate(8));
-    println!("{}", Base64::default().generate(8));
-}
