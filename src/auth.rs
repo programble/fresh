fa464953601dd69715c8911065755699832767ed
Curtis McEnroe <programble@gmail.com>
2016-02-05 00:55:37 -0500

    Update inth-oauth2 0.7.0

--- src/auth.rs
+++ src/auth.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 use std::io;
 use std::marker::PhantomData;
 
+use hyper;
 use inth_oauth2::{Client, ClientError, Token};
 use inth_oauth2::provider::Google;
 use inth_oauth2::token::{Bearer, Expiring};
@@ -12,29 +13,37 @@ use yup_oauth2;
 /// Google OAuth2 authenticators.
 pub trait Authenticator {
     /// Perform initial authentication.
-    fn authenticate(client: &Client<Google>, scope: &str) -> Result<Bearer<Expiring>, ClientError>;
+    fn authenticate(
+        client: &Client<Google>,
+        http: &hyper::Client,
+        scope: &str
+    ) -> Result<Bearer<Expiring>, ClientError>;
 }
 
 /// Simple authenticator via stdout and stdin.
 #[derive(Debug)]
 pub struct SimpleAuthenticator;
 impl Authenticator for SimpleAuthenticator {
-    fn authenticate(client: &Client<Google>, scope: &str) -> Result<Bearer<Expiring>, ClientError>
-    {
+    fn authenticate(
+        client: &Client<Google>,
+        http: &hyper::Client,
+        scope: &str
+    ) -> Result<Bearer<Expiring>, ClientError> {
         let auth_uri = try!(client.auth_uri(Some(scope), None));
         println!("{}", auth_uri);
 
         let mut code = String::new();
         try!(io::stdin().read_line(&mut code));
 
-        client.request_token(code.trim())
+        client.request_token(http, code.trim())
     }
 }
 
 /// Token cache for use with Google APIs.
-#[derive(Debug)]
+#[allow(missing_debug_implementations)]
 pub struct TokenCache<A: Authenticator> {
     client: Client<Google>,
+    http: hyper::Client,
     scope: String,
     token: Option<Bearer<Expiring>>,
     authenticator: PhantomData<A>,
@@ -44,11 +53,13 @@ impl<A: Authenticator> TokenCache<A> {
     /// Creates a token cache.
     pub fn new<S>(
         client: Client<Google>,
+        http: hyper::Client,
         scope: S,
         token: Option<Bearer<Expiring>>
     ) -> Self where S: AsRef<str> {
         TokenCache {
             client: client,
+            http: http,
             scope: scope.as_ref().to_owned(),
             token: token,
             authenticator: PhantomData,
@@ -58,8 +69,8 @@ impl<A: Authenticator> TokenCache<A> {
     /// Returns a valid token from cache, by refreshing, or through the `Authenticator`.
     pub fn token(&mut self) -> Result<&Bearer<Expiring>, ClientError> {
         let token = match self.token.take() {
-            Some(token) => try!(self.client.ensure_token(token)),
-            None => try!(A::authenticate(&self.client, &self.scope)),
+            Some(token) => try!(self.client.ensure_token(&self.http, token)),
+            None => try!(A::authenticate(&self.client, &self.http, &self.scope)),
         };
         self.token = Some(token);
         Ok(self.token.as_ref().unwrap())
