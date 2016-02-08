669ae22d23e36634b2c367a481c9d3d3aaeaac03
Curtis McEnroe <programble@gmail.com>
2016-02-07 23:16:04 -0500

    Implement Authenticator in fresh-cli

--- src/bin/fresh-cli.rs
+++ src/bin/fresh-cli.rs
@@ -1,11 +1,36 @@
 #[macro_use(crate_version)]
 extern crate clap;
+extern crate fresh;
+extern crate hyper;
+extern crate inth_oauth2;
 extern crate xdg;
 
+use std::io;
 use std::path::PathBuf;
 
 use clap::{App, AppSettings, Arg, SubCommand};
+use fresh::authenticator::Authenticator;
+use hyper::Client as HttpClient;
+use inth_oauth2::{Client as OAuth2Client, ClientError as OAuth2ClientError};
+use inth_oauth2::provider::Provider;
 use xdg::BaseDirectories;
+
+struct Prompt;
+impl<P: Provider> Authenticator<P> for Prompt {
+    fn authenticate(
+        oauth2: &OAuth2Client<P>,
+        http: &HttpClient,
+        scope: &str
+    ) -> Result<P::Token, OAuth2ClientError> {
+        let auth_uri = try!(oauth2.auth_uri(Some(scope), None));
+        println!("To authorize, open the following URL and paste the code below:\n{}", auth_uri);
+
+        let mut code = String::new();
+        try!(io::stdin().read_line(&mut code));
+
+        oauth2.request_token(http, code.trim())
+    }
+}
 
 fn main() {
     let xdg = BaseDirectories::with_prefix("fresh").unwrap();
@@ -48,11 +73,7 @@ fn main() {
         .subcommand(
             SubCommand::with_name("hackernews")
                 .about("Reset Hacker News password")
-                .arg(
-                    Arg::with_name("username")
-                        .help("Hacker News username")
-                        .required(true)
-                )
+                .arg(Arg::with_name("username").required(true).help("Username"))
         )
         .get_matches();
 
