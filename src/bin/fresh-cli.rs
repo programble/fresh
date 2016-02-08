282e1967c798294b703ceb98b8dddba17fc5666d
Curtis McEnroe <programble@gmail.com>
2016-02-07 23:01:26 -0500

    Fill in fresh-cli arguments

--- src/bin/fresh-cli.rs
+++ src/bin/fresh-cli.rs
@@ -15,23 +15,36 @@ fn main() {
         .version(crate_version!())
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .setting(AppSettings::GlobalVersion)
+        .args(&[
+            Arg::with_name("token_path")
+                .long("token").short("t").value_name("PATH")
+                .help("Path to OAuth 2.0 token JSON"),
 
-        .arg(
-            Arg::with_name("token")
-                .long("token")
-                .short("t")
-                .help("Path to OAuth 2.0 token JSON")
-                .global(true)
-                .value_name("PATH")
-        )
-        .arg(
+            Arg::with_name("generator")
+                .long("generator").short("g").value_name("GEN")
+                .possible_values(&["char", "hex", "base64"])
+                .help("Password generator"),
+
+            Arg::with_name("length")
+                .long("length").short("l").value_name("N")
+                .help("Password length"),
+
+            Arg::with_name("tries")
+                .long("tries").value_name("N")
+                .help("Number of inbox query retries"),
+
+            Arg::with_name("delay")
+                .long("delay").value_name("SECS")
+                .help("Delay between inbox query retries"),
+
+            Arg::with_name("no_archive")
+                .long("no-archive")
+                .help("Do not archive password reset message"),
+
             Arg::with_name("open")
-                .long("open")
-                .short("o")
+                .long("open").short("o")
                 .help("Open login page after reset")
-                .global(true)
-        )
-
+        ])
         .subcommand(
             SubCommand::with_name("hackernews")
                 .about("Reset Hacker News password")
@@ -41,14 +54,12 @@ fn main() {
                         .required(true)
                 )
         )
-
         .get_matches();
 
     let (subcommand, sub_matches) = matches.subcommand();
     let sub_matches = sub_matches.unwrap();
 
-    let token_path = matches.value_of("token")
-        .or(sub_matches.value_of("token"))
+    let token_path = matches.value_of("token_path")
         .map(PathBuf::from)
         .unwrap_or(xdg.place_config_file("token.json").unwrap());
 }
