c8dedcefee3c333ba96570d357a89dc70f5dfb63
Curtis McEnroe <programble@gmail.com>
2016-02-20 21:26:49 -0500

    Implement token loading and saving

--- src/bin/fresh-cli/main.rs
+++ src/bin/fresh-cli/main.rs
@@ -16,9 +16,39 @@ extern crate inth_oauth2;
 extern crate rustc_serialize;
 extern crate xdg;
 
+use std::path::PathBuf;
+
+use clap::{App, AppSettings, Arg, SubCommand};
+
 mod authenticator;
 mod generate;
 mod token_cache;
 
 fn main() {
+    let matches = App::new("Fresh CLI")
+        .about("Random password reset automation")
+        .version(crate_version!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::GlobalVersion)
+        .args(&[
+              Arg::with_name("token_path")
+                .long("token").short("t").value_name("PATH")
+                .help("Path to OAuth 2.0 token JSON"),
+        ])
+        .subcommand(
+            SubCommand::with_name("hackernews")
+                .about("Reset Hacker News password")
+                .arg(Arg::with_name("username").required(true).help("Username"))
+        )
+        .get_matches();
+
+    let token_path = matches.value_of("token_path")
+        .map(PathBuf::from)
+        .unwrap_or(token_cache::default_path());
+
+    let mut cache = token_cache::load(&token_path);
+
+    cache.authenticate().unwrap();
+
+    token_cache::save(&mut cache, &token_path);
 }
