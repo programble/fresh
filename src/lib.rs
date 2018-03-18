145269315ccf93e1f3184c8cb799526dc732acbc
Curtis McEnroe <programble@gmail.com>
2018-03-18 15:39:53 -0400

    Add generate_password and mailparse things

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,17 @@
 #[macro_use] extern crate failure;
+extern crate mailparse;
 extern crate reqwest;
 extern crate scraper;
+extern crate rand;
 
-use failure::Error;
-use reqwest::Client;
-use scraper::{Html, Selector};
+pub mod reset;
 
-pub trait Reset {
-    fn send_email(&self, client: &Client) -> Result<(), Error>;
-}
-
-pub struct HackerNews<'a>(pub &'a str);
-
-impl<'a> Reset for HackerNews<'a> {
-    fn send_email(&self, client: &Client) -> Result<(), Error> {
-        let html = client.get("https://news.ycombinator.com/forgot")
-            .send()?
-            .error_for_status()?
-            .text()?;
-        let doc = Html::parse_document(&html);
-        let sel = Selector::parse(r#"input[name="fnid"]"#).unwrap();
-        let fnid = doc.select(&sel)
-            .next()
-            .ok_or(format_err!("no fnid input"))?
-            .value()
-            .attr("value")
-            .ok_or(format_err!("no fnid value"))?;
-
-        let form = [
-            ("fnop", "forgot-password"),
-            ("fnid", fnid),
-            ("s", &self.0),
-        ];
-        client.post("https://news.ycombinator.com/x")
-            .form(&form)
-            .send()?
-            .error_for_status()?;
-        Ok(())
-    }
-}
-
-struct A {
-    reset: Box<Reset>,
+pub fn generate_password(len: usize) -> String {
+    use rand::distributions::{IndependentSample, Range};
+    let range = Range::new(32u8, 127);
+    let mut rng = rand::thread_rng();
+    std::iter::repeat(&range)
+        .map(|r| r.ind_sample(&mut rng) as char)
+        .take(len)
+        .collect()
 }
---
+++ src/reset.rs
@@ -0,0 +1,53 @@
+use failure::Error;
+use reqwest::Client;
+use scraper::{Html, Selector};
+use mailparse::ParsedMail;
+
+fn _object_safe(_: &Reset) { }
+pub trait Reset {
+    fn send_mail(&self, client: &Client) -> Result<(), Error>;
+    fn match_mail(&self, mail: &ParsedMail) -> bool;
+    fn set_password(
+        &self, client: &Client, mail: &ParsedMail, password: &str
+    ) -> Result<(), Error>;
+}
+
+pub struct HackerNews<'a>(pub &'a str);
+
+impl<'a> Reset for HackerNews<'a> {
+    fn send_mail(&self, client: &Client) -> Result<(), Error> {
+        let html = client.get("https://news.ycombinator.com/forgot")
+            .send()?
+            .error_for_status()?
+            .text()?;
+        let doc = Html::parse_document(&html);
+        let sel = Selector::parse(r#"input[name="fnid"]"#).unwrap();
+        let fnid = doc.select(&sel)
+            .next()
+            .ok_or(format_err!("no fnid input"))?
+            .value()
+            .attr("value")
+            .ok_or(format_err!("no fnid value"))?;
+
+        let form = [
+            ("fnop", "forgot-password"),
+            ("fnid", fnid),
+            ("s", &self.0),
+        ];
+        client.post("https://news.ycombinator.com/x")
+            .form(&form)
+            .send()?
+            .error_for_status()?;
+        Ok(())
+    }
+
+    fn match_mail(&self, _mail: &ParsedMail) -> bool {
+        false
+    }
+
+    fn set_password(
+        &self, _client: &Client, _mail: &ParsedMail, _password: &str
+    ) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
